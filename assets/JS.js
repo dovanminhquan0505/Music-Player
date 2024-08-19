4dad958135f1385c607d9015033ad92e5452c54f
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-19 14:11:18 +0700

    save button

--- assets/JS.js
+++ assets/JS.js
@@ -14,6 +14,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const PLAYER_STORAGE_KEY = 'PLAYER'; 
+
 const player = $('.player');
 const cd = $('.cd');
 const heading = $('header h2');
@@ -32,6 +34,7 @@ const app = {
     isPlaying: false,
     isRandom: false,
     isRepeat: false,
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
     songs: [
         {
             name: 'Những kẻ mộng mơ',
@@ -76,6 +79,10 @@ const app = {
             image: './assets/img/song7.png'
         }
     ],
+    setConfig: function(key, value){
+        this.config[key] = value;
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config))
+    },
     render:function(){
         const htmls = this.songs.map((song, index) => {
             return `<div class="song ${index === this.currentIndex ? 'active': ''}" data-index = ${index}> 
@@ -186,12 +193,14 @@ const app = {
         //Xử lý khi click vào random bài hát
         randomBtn.onclick = function(){
             _this.isRandom = !_this.isRandom;
+            _this.setConfig('isRandom', _this.isRandom);
             randomBtn.classList.toggle('active', _this.isRandom); // nếu israndom đúng thì add active, ko đúng thì remove active    
         }   
 
         //Xử lý khi repeat song khi audio ended
         repeatBtn.onclick = function(){
             _this.isRepeat = !_this.isRepeat;
+            _this.setConfig('isRepeat', _this.isRepeat); // lưu trữ lại isRepeat vào local storage
             repeatBtn.classList.toggle('active', _this.isRepeat); // nếu isRepeat đúng thì add active, ko đúng thì remove active
         }
 
@@ -242,6 +251,10 @@ const app = {
         cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
         audio.src = this.currentSong.path;
     },
+    loadConfig: function(){
+        this.isRandom = this.config.isRandom;
+        this.isRepeat = this.config.isRepeat;
+    },
     nextSongs: function(){
         this.currentIndex++;
         if(this.currentIndex >= this.songs.length){
@@ -262,6 +275,9 @@ const app = {
         this.loadCurrentSong();
     },
     start: function(){
+        //Gán cấu hình từ config vào ứng dụng
+        this.loadConfig();
+
         //Định nghĩa các thuộc tính cho Object
         this.defineProperties();
 
@@ -273,6 +289,10 @@ const app = {
         
         //Render playLists
         this.render();
+
+        //Hiển thị trạng thái ban đầu của button repeat và random
+        repeatBtn.classList.toggle('active', this.isRepeat); 
+        randomBtn.classList.toggle('active', this.isRandom);
     }
 };
 
